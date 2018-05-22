cc8ba5a3faccbdda7f8f7d83cb819c0450591083
talrota <tal.rota@gmail.com>
2018-05-22 22:50:21 +0300

    ex4

---
+++ app/src/main/java/talrota/post_pc_ex4/ImgurService.java
@@ -0,0 +1,20 @@
+package talrota.post_pc_ex4;
+
+import com.google.gson.JsonElement;
+
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Headers;
+import retrofit.http.Path;
+
+/**
+ * AppWell.org
+ * Created by dmitrijtrandin on 12.06.15.
+ */
+public interface ImgurService {
+
+    @Headers("Authorization: Client-ID " + Config.CLIENT_ID)
+    @GET("/album/{albumHash}")
+    void getAlbumImages(@Path("albumHash") String albumHash,  Callback<JsonElement> cb);
+}
